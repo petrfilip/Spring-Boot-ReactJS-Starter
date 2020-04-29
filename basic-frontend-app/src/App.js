a0ad3b2f83a7cf6c1af6ddc285d5d9ddbf1c0fe7
Petr Filip <petr.filip@tix.cz>
2020-04-29 19:35:17 +0200

    Working JWT with frontend

--- basic-frontend-app/src/App.js
+++ basic-frontend-app/src/App.js
@@ -1,36 +1,9 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import AppRouter from "./component/RouterComponent";
 
-class App extends Component {
-
-    state = {};
-
-        componentDidMount() {
-            this.dadJokes()
-        }
-
-    dadJokes = () => {
-        fetch('/api/dadjokes')
-            .then(response => response.text())
-            .then(message => {
-                this.setState({message: message});
-            });
-    };
-
-    render() {
-        return (
-            <div className="App">
-            <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h3 className="App-title">{this.state.message}</h3>
-            </header>
-            <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        </div>
-    );
-    }
+function App() {
+  return (<AppRouter/>);
 }
 
 export default App;
---
+++ src/main/java/e/the/awesome/springreactcomboapp/controller/AuthenticationController.java
@@ -0,0 +1,47 @@
+package e.the.awesome.springreactcomboapp.controller;
+
+import e.the.awesome.springreactcomboapp.config.JwtTokenUtil;
+import e.the.awesome.springreactcomboapp.model.ApiResponse;
+import e.the.awesome.springreactcomboapp.model.AuthToken;
+import e.the.awesome.springreactcomboapp.model.LoginUser;
+import e.the.awesome.springreactcomboapp.model.User;
+import e.the.awesome.springreactcomboapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/token")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/generate-token", method = RequestMethod.POST)
+    public ApiResponse<AuthToken> generateToken(@RequestBody LoginUser loginUser) throws AuthenticationException {
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUser.getUsername(), loginUser.getPassword()));
+        final User user = userService.findOne(loginUser.getUsername());
+        final String token = jwtTokenUtil.generateToken(user);
+        return new ApiResponse<>(200, "success",new AuthToken(token, user.getUsername()));
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    public ApiResponse<Void> logout() throws AuthenticationException {
+        return new ApiResponse<>(200, "success",null);
+    }
+
+}
